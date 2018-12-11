5836c11b12e72d30e62ed63a58c3c53fd765bfaa
Sergio García Santos <sergio.garciasantos@travelport.com>
2018-12-11 14:04:58 +0100

    build: add react-hooks eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,31 @@
 module.exports = {
-    parser: 'babel-eslint',
-    env: {
-      browser: true,
-      commonjs: true,
-      es6: true,
-      node: true,
-      jest: true,
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
     },
-    extends: ['eslint:recommended', 'plugin:react/recommended'],
-    parserOptions: {
-      ecmaFeatures: {
-        experimentalObjectRestSpread: true,
-        jsx: true,
-      },
-      sourceType: 'module',
-    },
-    plugins: ['react'],
-    rules: {
-      'react/prop-types': ['off'],
-      indent: ['error', 2, { SwitchCase: 1 }],
-      'linebreak-style': ['error', 'unix'],
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
-      'no-console': ['warn', { allow: ['info', 'error'] }],
-      'arrow-parens': ['error', 'always'],
-      'comma-dangle': ['error', 'always-multiline'],
-      'max-len': ['error', { code: 100 }],
-    },
-  };
-  
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react/prop-types': ['off'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-console': ['warn', { allow: ['info', 'error'] }],
+    'arrow-parens': ['error', 'always'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'max-len': ['error', { code: 100 }],
+  },
+};
